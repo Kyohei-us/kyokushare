2dab3027781c413ae26cc77202c4f21581bc105c
Kyohei-us <kyohei1212000@gmail.com>
2022-11-21 02:02:46 +0900

    Add login functionality: beta

--- src/frontend.ts
+++ src/frontend.ts
@@ -1,31 +1,65 @@
-import { Router } from "express";
-import { getAllKyokus, getArtistById, getKyokuFullInfoByKyokuId } from "./dbservices";
+import { NextFunction, Request, Response, Router } from "express";
+import {
+  getAllKyokus,
+  getArtistById,
+  getKyokuFullInfoByKyokuId,
+} from "./dbservices";
 
 const frontendRouter = Router();
 
 frontendRouter.get("/", async (req, res) => {
-    const skip = req.query.skip ? Number(req.query.skip) : 0;
-    const take = req.query.take ? Number(req.query.take) : undefined;
-    const kyokus = await getAllKyokus(skip, take);
-    const data = {kyokus};
-    res.render("./index.ejs", data);
-})
+  const skip = req.query.skip ? Number(req.query.skip) : 0;
+  const take = req.query.take ? Number(req.query.take) : undefined;
+  const kyokus = await getAllKyokus(skip, take);
+
+  const data = { kyokus, username: req.session.username };
+  res.render("./index.ejs", data);
+});
 
 frontendRouter.get("/commentsByKyokuId/:id", async (req, res) => {
-    const kyoku = await getKyokuFullInfoByKyokuId(Number(req.params.id));
-    const data = {
-        kyoku,
-        comments: kyoku?.comments
-    };
-    res.render("./comments.ejs", data);
-})
+  const kyoku = await getKyokuFullInfoByKyokuId(Number(req.params.id));
+  const data = {
+    kyoku,
+    comments: kyoku?.comments,
+  };
+  res.render("./comments.ejs", data);
+});
 
 frontendRouter.get("/artist/:id", async (req, res) => {
-    const artist = await getArtistById(Number(req.params.id));
-    const data = {
-        artist
+  const artist = await getArtistById(Number(req.params.id));
+  const data = {
+    artist,
+  };
+  res.render("./artist.ejs", data);
+});
+
+frontendRouter.get("/login", async (req, res) => {
+  res.render("login");
+});
+
+frontendRouter.post(
+  "/login", (req, res) => {
+    console.log("logging in...");
+    console.log(req.body);
+    if (req.body.username == "test") {
+        req.session.username = req.body.username;
+        req.session.save();
+        console.log("logged in!");
+        res.redirect("/");
+    } else {
+        console.log("login failed!");
+        res.redirect("/login");
     }
-    res.render("./artist.ejs", data)
-})
+  }
+);
+
+frontendRouter.get("/logout", function (req, res) {
+  if (req.session.username) {
+    req.session.username = "";
+    req.session.save();
+    console.log("logged out!");
+    res.redirect("/login");
+  }
+});
 
 export default frontendRouter;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,41 @@
 import express from "express";
-import cors from "cors";
 import kyokusRouter from "./kyokus";
 import commentsRouter from "./comments";
 import frontendRouter from "./frontend";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+declare global {
+  namespace Express {
+    interface User {
+      username: string;
+      password: string;
+    }
+  }
+}
+
+declare module 'express-session' {
+  interface SessionData {
+      username: string
+  }
+}
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.set("view engine", "ejs");
+app.set("trust proxy", 1);
+
+app.use(
+  session({
+    secret: "yahaa",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use("/api/kyokus", kyokusRouter);
 
